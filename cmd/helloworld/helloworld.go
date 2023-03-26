bf2c6741ca968f89ece842460c62f90f9026cf13
Chris Wyburn-Powell <chriswyburn-powell@chriss-mbp.lan>
2023-03-26 15:50:53 -0600

    add helloworld

---
+++ cmd/helloworld/helloworld.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/chrisrwp/gaitway/internal/arithmetic"
+)
+
+func main() {
+	fmt.Println("hello world!")
+	fmt.Println(arithmetic.Add(2, 3))
+}
---
+++ internal/arithmetic/helper.go
@@ -0,0 +1,5 @@
+package arithmetic
+
+func Add(x, y int) int {
+	return x + y
+}
